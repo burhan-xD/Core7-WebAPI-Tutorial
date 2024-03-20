076b6736b8366f7317934be2ae66ddb8bd0625f1
Burhan Ozsahinoglu <burhanozsahinoglu@windowslive.com>
2024-03-20 16:53:53 +0300

    Mediator yapısı uygulandı. Product için listeleme işlemi yapıldı

---
+++ Core/WebApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.Interfaces.UnitOfWorks;
+using WebApi.Domain.Entities;
+
+namespace WebApi.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQueryRequest, IList<GetAllProductQueryResponse>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public GetAllProductQueryHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<IList<GetAllProductQueryResponse>> Handle(GetAllProductQueryRequest request, CancellationToken cancellationToken)
+        {
+            var products = await unitOfWork.GetReadRepository<Product>().GetAllAsync();
+
+            List<GetAllProductQueryResponse> response = new();
+
+            foreach (var product in products)
+            {
+                response.Add(new GetAllProductQueryResponse         // Automapper was not used.
+                {
+                    Title = product.Title,
+                    Description = product.Description,
+                    Discount = product.Discount,
+                    Price = product.Price - (product.Price * product.Discount / 100),
+                });
+            }
+
+            return response;
+        }
+    }
+}
