5dc29c826c8f01a206a80056a72d8d210c72ff6f
Burhan Ozsahinoglu <burhanozsahinoglu@windowslive.com>
2024-03-23 23:12:23 +0300

    Global exception handling uygulaması yapıldı

--- Core/WebApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductQueryHandler.cs
+++ Core/WebApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductQueryHandler.cs
@@ -44,9 +44,10 @@ namespace WebApi.Application.Features.Products.Queries.GetAllProducts
             var map = mapper.Map<GetAllProductQueryResponse, Product>(products);
             foreach (var item in map)
                 item.Price -= (item.Price * item.Discount / 100);
-            
 
-            return map;
+
+            //return map;
+            throw new Exception("Hata_xD");
         }
     }
 }
