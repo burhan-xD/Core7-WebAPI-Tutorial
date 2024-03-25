b54fee8e8bcc6a957cd1bd15f25133492b3c2dee
Burhan Ozsahinoglu <burhanozsahinoglu@windowslive.com>
2024-03-25 22:43:34 +0300

    Login işlemleri, AccesToken, Authorize uygulaması yapıldı-Mediatr ve JWT

---
+++ Core/WebApi.Application/Features/Auth/Exceptions/EmailAddressShouldBeValidException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.Bases;
+
+namespace WebApi.Application.Features.Auth.Exceptions
+{
+    public class EmailAddressShouldBeValidException : BaseExceptions
+    {
+        public EmailAddressShouldBeValidException() : base("Eposta adresi bulunamadı..._xD") { }
+    }
+}
